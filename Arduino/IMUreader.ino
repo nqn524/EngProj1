64d65a502d2eb30778ffbad8b172bccc3d01d8ce
gtj520-Sul <gtj520@york.ac.uk>
2026-04-22 21:04:56 +0100

    Update IMUreader.ino
    Should work :)

--- Arduino/IMUreader.ino
+++ Arduino/IMUreader.ino
@@ -1,2 +1,39 @@
-void IMUreader() {
+
+#include <math.h> // need for truncating
+#include "Arduino_BMI270_BMM150.h" // the main library
+
+struct pseudoArray
+{
+  float x;
+  float y;
+  float z;
+};
+
+pseudoArray IMUreader() 
+{
+  IMU.begin();
+  pseudoArray position;
+  if (IMU.accelerationAvailable()) 
+  {
+    IMU.readAcceleration(position.x, position.y, position.z); // getting the data
+  }
+
+  position.x = trunc((position.x*100) + 5000000); // editing the value to correct parameters
+  position.y = trunc((position.y*100) + 5000000);
+  position.z = trunc((position.z*100) + 5000000);
+  if (position.x < 0)
+  {
+    position.x = 0;
+  }
+    if (position.y < 0)
+  {
+    position.y = 0;
+  }
+    if (position.z < 0)
+  {
+    position.z = 0;
+  }
+
+  return position;
+
 }
