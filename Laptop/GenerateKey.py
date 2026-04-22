844cabe93fac94ae85d841a947249bfd06b76ca2
gtj520-Sul <gtj520@york.ac.uk>
2026-04-22 23:03:16 +0100

    Update GenerateKey.py

--- Laptop/GenerateKey.py
+++ Laptop/GenerateKey.py
@@ -1,4 +1,16 @@
 def GenerateKey():
+  p = GeneratePrime(1)
+  q = GeneratePrime(2)
+  KeyN = p * q
+  k = (p-1) * (q-1)
+  
+  KeyE = KeyN - 1
+  while (KeyE < 1) or ( gcd(keyE, k) != 1): #generating a suitable keyE
+      n = 5
+      keyE = GeneratePrime(i)
+      n = n+1 #Basically going randomly through suitable primes until one sticks
+      
+  KeyD = pow(KeyE, -1, K) #Functon performs modular inverse on KeyE (mod k) to find a suitable keyD
   deleteMe = True
 
 def GeneratePrime(num):
