4c8bc3f7b4b867c7b744f5231f149a82de60af33
nqn524 <nqn524@york.ac.uk>
2026-04-22 17:22:18 +0100

    Update FFT.py

--- Laptop/FFT.py
+++ Laptop/FFT.py
@@ -4,8 +4,29 @@ def FFT(data, FREQ):
     vlaues = np.abs(np.fft.rfft(data))
     freqs = np.fft.rfftfreq(len(data), 1/FREQ)
 
-    return freqs, vlaues
+    return _removeLowerMags(vlaues, freqs)
+
+def _maxVal(data):
+    biggest = data[0]
+
+    for i in range(1, len(data)):
+        if biggest < data[i]:
+            biggest = data[i]
+
+    return biggest
 
 
-if __name__ == "__main__":
-    print()
+def _removeLowerMags(vals, freqs):
+    biggest = _maxVal(vals)
+
+    returnVals = []
+    returnFreq = []
+
+    for i in range(0, len(vals)):
+        if vals[i] > 0.05 * biggest:
+            returnVals.append(float(vals[i]))
+            returnFreq.append(float(freqs[i]))
+
+    print(returnVals, "   ", returnFreq)
+
+    return returnVals, returnFreq
