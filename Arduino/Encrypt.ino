f144d7a0b682c933ae1fa667b12df0db33a50ed7
nathanjayaguilar <nathan.jay.aguilar@gmail.com>
2026-04-28 14:46:43 +0100

    Update Encrypt.ino

--- Arduino/Encrypt.ino
+++ Arduino/Encrypt.ino
@@ -1,9 +1,16 @@
-int encryptMessage(int PlainText, int PublicKeyE, int PublicKeyN) {
-  int CipherText = 1;
-  
-  for (int i = 0; i < PublicKeyE; i++) {
-    CipherText = (CipherText * PlainText) % PublicKeyN;
+uint64_t encryptData(uint64_t PlainText, uint64_t PublicKeyE, uint64_t PublicKeyN) {
+  uint64_t CipherText = 1;
+
+  PlainText = PlainText % PublicKeyN;
+
+  while (PublicKeyE > 0) {
+    if (PublicKeyE % 2 == 1) {
+      CipherText = (CipherText * PlainText) % PublicKeyN;
+    }
+
+    PlainText = (PlainText * PlainText) % PublicKeyN;
+    PublicKeyE = PublicKeyE / 2;
   }
-  
+
   return CipherText;
 }
