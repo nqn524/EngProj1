80aa4bbd5afeac8ff22051de2698b5ee116b9912
nathanjayaguilar <nathan.jay.aguilar@gmail.com>
2026-05-09 21:50:38 +0100

    Update ReadFile.py

--- Laptop/ReadFile.py
+++ Laptop/ReadFile.py
@@ -1,2 +1,22 @@
-def ReadFile(filename):
-  return [0], [0], [0], [0]
+def read_data(file_name):
+    x_values = []
+    y_values = []
+    z_values = []
+    time = []
+
+    try:
+        with open(file_name, 'r') as file:
+            next(file)
+            
+            for line in file:
+                parts = line.strip().split(',')
+                
+                x_values.append(float(parts[0]))
+                y_values.append(float(parts[1]))
+                z_values.append(float(parts[2]))
+                time.append(float(parts[3]))
+                
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+
+    return x_values, y_values, z_values, time
